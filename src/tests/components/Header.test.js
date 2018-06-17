73c8261b4e2f8dfb5d718574ef828547ed1a93db
Andy Smith <work@andysmith.is>
2018-06-18 06:34:14 +1000

    added login page, auth, private routes

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,17 +1,21 @@
 // react-test-renderer
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
+
+let wrapper, startLogout;
+
+beforeEach(() => {
+  startLogout = jest.fn();
+  wrapper = shallow(<Header startLogout={startLogout} />)
+});
 
 test('should render header correctly', () => {
-  const wrapper = shallow(<Header />)
   expect(wrapper).toMatchSnapshot();
- 
-  // expect(wrapper.find('h1').text()).toBe('Expensify');
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
-  // console.log(renderer.getRenderOutput());
 });
 
 
+test('should call startLogout on button click', () =>{
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
+});
