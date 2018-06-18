652ab530e10203f16edbb862787e5215d26bea0e
Andy Smith <work@andysmith.is>
2018-06-19 07:06:17 +1000

    styling the app

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -5,15 +5,17 @@ import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
 export const ExpenseSummary = (props) => (
-  <div>
-    {
-      (props.expenseCount > 1 || props.expenseCount === 0) ? 
-      (
-        <p>Viewing {props.expenseCount} expenses totalling {numeral(props.expensesTotal/100).format('$0,0.00')}</p>
-      ) : (
-        <p>Viewing 1 expense totalling {numeral(props.expensesTotal/100).format('$0,0.00')}</p>
-      )
-    } 
+  <div className="page-header">
+    <div className="content-container">
+      {
+        (props.expenseCount > 1 || props.expenseCount === 0) ? 
+        (
+          <h1 className="page-header__title">Viewing {props.expenseCount} expenses totalling {numeral(props.expensesTotal/100).format('$0,0.00')}</h1>
+        ) : (
+          <h1 className="page-header__title">Viewing 1 expense totalling {numeral(props.expensesTotal/100).format('$0,0.00')}</h1>
+        )
+      }
+    </div> 
   </div>
 );
 
